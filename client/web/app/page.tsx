1959f07b863d91bf56085d9a22917dacbe1ebc44
Shem Joshua M. Dumpor <dumporshemjoshua@gmail.com>
2026-02-12 02:50:48 +0800

    hi

--- client/web/app/page.tsx
+++ client/web/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
       <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
+            Shem
           </span>
         </Snippet>
       </div>
