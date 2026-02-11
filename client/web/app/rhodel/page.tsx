962e55a0db3cfb83ed3f5190dcc6bf8c29b05733
rhodythesmoothie <rhodelbadiango@gmail.com>
2026-02-12 02:40:52 +0800

    o lage

--- client/web/app/rhodel/page.tsx
+++ client/web/app/rhodel/page.tsx
@@ -2,8 +2,18 @@ import { title } from "@/components/primitives";
 
 export default function PricingPage() {
   return (
-    <div>
-      <h1 className={title()}>Pricing</h1>
+    <div className="p-6 bg-black text-white">
+      <h1 className={`${title()} mb-4`}>
+        Xydric Bayot
+      </h1>
+
+      <h2 className="mb-2 text-xl">
+        Gift Giving
+      </h2>
+      
+      <p className="mb-4">
+        Xydric Cleve Niere is a transwoman who lives in Paknaan, Mandaue and is currently a student at University of Cebu - Lapu-lapu and Mandaue. She is known as a perfume specialist specifically for Victoria’s Secret and Bath and Body Works.
+      </p>
     </div>
   );
 }
